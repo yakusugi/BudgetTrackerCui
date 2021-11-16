75d78e38a1b5331d91a9c53e56d505bc88d1efc8
yosuke <yosuke@yosuke-Galago-Pro>
2021-11-16 10:00:01 +0900

    Test commit

---
+++ BudgetTrackerCui/src/test_package/TestMain.java
@@ -0,0 +1,13 @@
+package test_package;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Test");
+		System.out.println("Test");
+		
+	}
+
+}

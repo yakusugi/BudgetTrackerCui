de3d6bb91587a0bae2b99ffb39135cd22b2945fb
yosuke <yosuke@yosuke-Galago-Pro>
2021-11-21 12:54:33 +0900

    nov 21 2021

--- BudgetTrackerCui/src/CSVInsert/CSVInsert.java
+++ BudgetTrackerCui/src/CSVInsert/CSVInsert.java
@@ -1,20 +1,32 @@
 package CSVInsert;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Properties;
 
 import static java.lang.Integer.parseInt;
 
 public class CSVInsert {
-    public static void main(String[] args) {
-        String jdbcUrl="jdbc:mysql://192.168.0.45:3306/testdb";
-        String username="host_user";
-        String password="Unity0216Access!";
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+    	
+    	Properties props = new Properties();
+        props.load(new FileInputStream("/home/yosuke/git/BudgetTrackerCui/BudgetTrackerCui/sql/config_budgettracker.properties"));
 
-        String filePath="/home/yosuke/Documents/GDP.csv";
+        String user = props.getProperty("user");
+        String password = props.getProperty("password");
+        String dburl = props.getProperty("dburl");
+    	
+//        String jdbcUrl="jdbc:mysql://192.168.0.45:3306/testdb";
+//        String username="host_user";
+//        String password="Unity0216Access!";
+
+        String filePath="/home/yosuke/Downloads/budget_table.csv";
 
         int batchSize=20;
 
@@ -22,10 +34,10 @@ public class CSVInsert {
 
 
         try{
-            connection= DriverManager.getConnection(jdbcUrl,username,password);
+            connection= DriverManager.getConnection(dburl,user,password);
             connection.setAutoCommit(false);
 
-            String sql="insert into gdp_table(country_code,ranking,country_name,gdp) values(?,?,?,?)";
+            String sql="insert into budget_table(id,Date,StoreName,ProductName, ProductType, Price) values(?,?,?,?,?,?)";
 
             PreparedStatement statement=connection.prepareStatement(sql);
 
@@ -38,15 +50,19 @@ public class CSVInsert {
             while ((lineText=lineReader.readLine())!=null){
                 String[] data=lineText.split(",");
 
-                String country_code=data[0];
-                String ranking=data[1];
-                String country_name=data[2];
-                String gdp=data[3];
+                String id=data[0];
+                String Date=data[1];
+                String StoreName=data[2];
+                String ProductName=data[3];
+                String ProductType=data[4];
+                String Price=data[5];
 
-                statement.setString(1,country_code);
-                statement.setInt(2,parseInt(ranking));
-                statement.setString(3,country_name);
-                statement.setInt(4,parseInt(gdp));
+                statement.setInt(1,parseInt(id));
+                statement.setString(2,parseString(Date));
+                statement.setString(3,StoreName);
+                statement.setString(4,ProductName);
+                statement.setString(5,ProductType);
+                statement.setInt(6,parseInt(Price));
                 statement.addBatch();
                 
                 if(count%batchSize==0){
@@ -67,4 +83,14 @@ public class CSVInsert {
         }
 
     }
+
+	private static String parseDate(String date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static String parseString(String date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
